4367b37079ae3e1be41231f48392e399600782a2
aspy <aspyraza2011@gmail.com>
2024-01-20 10:19:47 -0800

    add new folder

---
+++ start1.js
@@ -0,0 +1,74 @@
+//js-variable-dynamically types
+
+// var name = "vikas"
+// var age = "22"
+// var height = "5.8"
+// var weight = "54.300"
+
+
+// console.log("my name is", name)
+// console.log(`my name is ${name} and my age is ${age}`)
+// console.log(`my height is ${height} and my weight is ${weight}`)
+
+//conditionals
+// if else else if switch 
+
+// var age = 25
+// if(age>26){
+//      console.log("you can vote")
+// }
+// else{
+//     console.log("you can't vote")
+// }
+
+// var age = 45;
+
+// if(age<18){
+//     console.log("go to scool")
+// }
+// else if(age>18 && age<22){
+//     console.log("go to college")
+// }
+// else if(age>22 && age <25){
+//     console.log("focus on career")
+// }
+// else if(age>25 && age<50){
+//     console.log("go to office")
+// }
+// else{
+//     console.log("go to home")
+// }
+
+// var age = 28;
+
+// if(age<18){
+//     console.log("go to school")
+// }
+// else if(age>18 && age<23){
+//     console.log("go to college")
+// }
+// else if(age>23 && age<30){
+//     console.log("go to office")
+// }
+// else{
+//     console.log("go to party")
+// }
+
+// var myintrostring = `my name is aspyraza 
+// my age is 22 
+// my college name is vocational studies`
+// console.log(myintrostring)
+// document.write(myintrostring)
+
+var age = 32;
+
+if(age<18){
+    console.log("go to school")
+}
+else if(age>18 && age<35){
+    console.log("go to office")
+}
+else{
+    console.log("go to mandir")
+}
+
